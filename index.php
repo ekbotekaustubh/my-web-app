f8df51a3a74e50940377c49004e17c264c6fde4b
kaustubh <kaustubh.ekbote@agiliron.com>
2025-03-29 09:05:31 +0530

    Sign up form html

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Sign Up</title>
+    </head>
+    <body>
+    <table>
+        <tr>
+            <th>First Name</th>
+            <td><input type="text" name="firstName" id="firstName" value=""> </td>
+        </tr>
+        <tr>
+            <th>Last Name</th>
+            <td><input type="text" name="lastName" id="lastName" value=""> </td>
+        </tr>
+        <tr>
+            <th>Email</th>
+            <td><input type="text" name="email" id="email" value=""> </td>
+        </tr>
+        <tr>
+            <th>Username</th>
+            <td><input type="text" name="username" id="username" value=""> </td>
+        </tr>
+        <tr>
+            <th>Password</th>
+            <td><input type="password" name="password" id="password" value=""> </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" name="submit" id="submit" value="Submit">&nbsp;
+                <input type="reset" name="reset" id="reset" value="Reset">
+            </td>
+        </tr>
+    </table>
+    </body>
+</html>
